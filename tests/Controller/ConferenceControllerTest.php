d7e3ee1ed334cd8d64e54798efb0a00eb5821ff1
l37sg0 <petar_1v@abv.bg>
2023-11-25 15:03:42 +0200

    Going Asynk, flagging comments

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Panther\PantherTestCase;
 
 class ConferenceControllerTest extends PantherTestCase
@@ -24,11 +26,17 @@ class ConferenceControllerTest extends PantherTestCase
         $client->submitForm('Submit', [
             'comment_form[author]'  => 'Fabien',
             'comment_form[text]'    => 'Some feedback from an automated functional test',
-            'comment_form[email]'   => 'me@automated.ed',
+            'comment_form[email]'   => $email = 'me@automated.ed',
             'comment_form[photo]'   => dirname(__DIR__, 2) . '/public/images/under-construction.gif'
         ]);
         
         $this->assertResponseRedirects();
+
+        // simulate comment validation
+        $comment = self::getContainer()->get(CommentRepository::class)->findOneByEmail($email);
+        $comment->setState('published');
+        self::getContainer()->get(EntityManagerInterface::class)->flush();
+
         $client->followRedirect();
         $this->assertSelectorExists('div:conteind("There are 2 comments")');
     }
